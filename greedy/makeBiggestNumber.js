359c8852a3ab7163962083ad95f1a97575e01ff6
itx_doctorgu <itx_doctorgu@woowafriends.com>
2021-12-21 09:45:16 +0900

    use from instead of slice to be faster

--- greedy/makeBiggestNumber.js
+++ greedy/makeBiggestNumber.js
@@ -1,35 +1,6 @@
 'use strict'
 const { runTest } = require('../common/runTest')
 
-function getCombination(
-  list,
-  count,
-  start = 0,
-  info = { items: [], result: 0 }
-) {
-  if (info.items.length >= count) {
-    const item = parseInt(info.items.join(''))
-    // console.log(item, info.result)
-    if (item > info.result) {
-      info.result = item
-      // console.log(info.result)
-    }
-
-    return
-  }
-
-  for (let i = start; i < list.length; i++) {
-    const cur = list[i]
-    info.items.push(cur)
-
-    getCombination(list, count, i + 1, info)
-
-    info.items.pop()
-  }
-
-  return info.result
-}
-
 /*
 --summary
 Get largest and trim to exclude
@@ -37,18 +8,16 @@ Get largest and trim to exclude
 const list = [ 1, 9, 2, 4 ]
 const result = []
 
-getLargestAndTrim(list, 3, result)
-// list: [ 2, 4 ]
-// result: [ 9 ]
+from = getLargestAndTrim(list, 0, 2, result)
+// from: 2, result: [ 9 ]
 
-getLargestAndTrim(list, 2, result)
-// list: []
-// result: [ 9, 4 ]
+from = getLargestAndTrim(list, 2, 3, result)
+// from: 4, result: [ 9, 4 ]
 */
-function getLargestAndTrim(list, to, result = []) {
-  let max = 0
+function getLargestAndTrim(list, from, to, result = []) {
+  let max = -1
   let index = -1
-  for (let i = 0; i < to; i++) {
+  for (let i = from; i <= to; i++) {
     const item = list[i]
     if (item > max) {
       max = item
@@ -56,10 +25,9 @@ function getLargestAndTrim(list, to, result = []) {
     }
   }
 
-  list = list.slice(index + 1)
   result.push(max)
 
-  return list
+  return index + 1
 }
 
 function solution(number, k) {
@@ -67,8 +35,10 @@ function solution(number, k) {
   let count = list.length - k
 
   const result = []
+  let from = 0
   while (count >= 1) {
-    list = getLargestAndTrim(list, list.length - count + 1, result)
+    const to = list.length - count
+    from = getLargestAndTrim(list, from, to, result)
     count -= 1
   }
 
@@ -137,6 +107,19 @@ module.exports = {
 테스트 10 〉	실패 (시간 초과)
 테스트 11 〉	통과 (0.11ms, 30MB)
 테스트 12 〉	통과 (0.09ms, 30.3MB)
+
+테스트 1 〉	통과 (0.12ms, 30.2MB)
+테스트 2 〉	통과 (0.14ms, 30.4MB)
+테스트 3 〉	통과 (0.21ms, 30.1MB)
+테스트 4 〉	통과 (0.34ms, 30.4MB)
+테스트 5 〉	통과 (0.56ms, 30.3MB)
+테스트 6 〉	통과 (8.55ms, 33MB)
+테스트 7 〉	통과 (20.35ms, 33.8MB)
+테스트 8 〉	통과 (114.68ms, 35.9MB)
+테스트 9 〉	통과 (46.21ms, 51.1MB)
+테스트 10 〉	통과 (4350.10ms, 51MB)
+테스트 11 〉	통과 (0.14ms, 30.2MB)
+테스트 12 〉	통과 (0.12ms, 30.3MB)
 */
 
 // node -e "require('./greedy/makeBiggestNumber.js').test()"
