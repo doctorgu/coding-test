365c69e7efbc7f7dc15c44bc4e95c180b3650602
itx_doctorgu <itx_doctorgu@woowafriends.com>
2021-12-24 09:10:54 +0900

    getCost return wrogn result

--- greedy/connectingIsland.js
+++ greedy/connectingIsland.js
@@ -1,39 +1,77 @@
 'use strict'
 const { runTest } = require('../common/runTest')
 
+function getAllPossibleRoutes(list) {
+  const ret = []
+  for (let i = 0; i < list.length; i++) {
+    for (let j = i + 1; j < list.length; j++) {
+      ret.push([list[i], list[j]])
+    }
+  }
+  return ret
+}
+
+function getCost(costs, fromP, toP, costsFound = { temp: 0, final: 0 }) {
+  const froms = costs.filter(([fromC]) => fromC === fromP)
+
+  for (let i = 0; i < froms.length; i++) {
+    const [, toC, cost] = froms[i]
+    costsFound.temp += cost
+    if (toC === toP) {
+      costsFound.final = Math.min(costsFound.final, costsFound.temp)
+    } else {
+      getCost(costs, toC, toP, costsFound)
+    }
+  }
+
+  return costsFound.final
+}
+
+function getCheapest(possibles, costs) {
+  possibles.forEach(([fromP, toP]) => {
+    const cost = getCost(costs, fromP, toP)
+    console.log(fromP, toP, cost)
+  })
+}
+
 function solution(n, costs) {
   costs.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]))
-  console.log(costs)
+
+  const list = [...Array(n).keys()]
+  const possibles = getAllPossibleRoutes(list)
+  const price = getCheapest(possibles, costs)
 
   var answer = 0
   return answer
 }
 
 function test() {
-  // runTest(
-  //   solution,
-  //   4,
-  //   [
-  //     [0, 1, 1],
-  //     [0, 2, 2],
-  //     [1, 2, 5],
-  //     [1, 3, 1],
-  //     [2, 3, 8],
-  //   ],
-  //   4
-  // )
   runTest(
     solution,
     4,
     [
-      [0, 2, 2],
       [0, 1, 1],
-      [2, 3, 8],
+      [0, 2, 2],
       [1, 2, 5],
       [1, 3, 1],
+      [2, 3, 8],
     ],
     4
   )
+
+  // // Test sorting
+  // runTest(
+  //   solution,
+  //   4,
+  //   [
+  //     [0, 2, 2],
+  //     [0, 1, 1],
+  //     [2, 3, 8],
+  //     [1, 2, 5],
+  //     [1, 3, 1],
+  //   ],
+  //   4
+  // )
 }
 
 module.exports = {
